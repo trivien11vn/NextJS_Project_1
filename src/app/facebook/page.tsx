09511475c40f5f0b0ec129a5675310c76d80f175
trivien11vn <triviencoderr@gmail.com>
2024-12-29 03:07:56 +0700

    Navigation

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -1,6 +1,17 @@
+'use client'
+import { useRouter } from "next/navigation"
 const Admin = () => {
+    const router = useRouter()
+
+    const handleOnClick = () => {
+        router.push("/")
+    }
+
     return <>
         Facebook
+        <div>
+            <button onClick={() => { handleOnClick() }}>Back to Home</button>
+        </div>
     </>
 }
 
